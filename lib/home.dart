5b033cd39afcd7d449ce4f10cb7230476e887c6c
Afna sherin <afnasherin34@gmail.com>
2023-05-08 11:37:41 +0530

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:facebook/widgets/appBarButton.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Text(
+            'facebook',
+            style: TextStyle(
+              color: Colors.lightBlue,
+              fontSize: 26,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          actions: [
+            AppBarButton(
+              buttonIcon: Icons.search,
+              buttonAction: () {
+                print('go to button icon');
+              },
+            ),
+            AppBarButton(
+              buttonIcon: Icons.message,
+              buttonAction: () {
+                print('go to message icon');
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
